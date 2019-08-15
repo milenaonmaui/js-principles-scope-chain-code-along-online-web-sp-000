06b25a97c7530ff9cee57166476fa83c138e2693
Milena Kari <milenakari@yahoo.com>
2019-08-15 19:16:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 /* Your code goes here */
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+  customerName=customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+  bestCustomer = 'not bob';
+}
+
+function overWriteBestCustomer(){
+  bestCustomer='maybe bob';
+}
+
+const leastFavoriteCustomer = 'bob'
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'not bob';
+}
