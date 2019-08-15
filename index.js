fdc73a209c9c126420fe08ab60507989734bab38
Milena Kari <milenakari@yahoo.com>
2019-08-15 19:24:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ function changeLeastFavoriteCustomer(){
 
 function attemptTwoFavoriteCustomers(){
   let favoriteCustomer = 'jeff';
-  let favoriteCustomer = 'bobby';
+  favoriteCustomer = 'bobby';
 }
