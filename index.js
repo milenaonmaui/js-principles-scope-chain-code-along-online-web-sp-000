88c164bca58de0b82605f6302da5133876516614
Milena Kari <milenakari@yahoo.com>
2019-08-15 19:23:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function changeLeastFavoriteCustomer(){
 }
 
 function attemptTwoFavoriteCustomers(){
-  const favoriteCustomer = 'jeff';
-  favoriteCustomer = 'bobby';
+  let favoriteCustomer = 'jeff';
+  let favoriteCustomer = 'bobby';
 }
